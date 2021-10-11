cf13d220dcd3c2336f37c3374483fc014a9f368a
Gian <giansingh4710@gmail.com>
2021-10-11 13:27:57 -0400

    files working

--- README.md
+++
@@ -1,1 +0,0 @@
-# gurmatveecharPY2
---
+++ test.py
@@ -0,0 +1,8 @@
+# import tkinter
+from six.moves import urllib
+
+#C:/Python27/python.exe test.py
+# C:/Python27/Scripts/pip2.exe install six
+
+urllib.request.urlretrieve('https://automatetheboringstuff.com/files/rj.txt',"./test.txt")
+print("Hi")
